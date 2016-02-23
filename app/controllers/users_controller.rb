15fe6ce2bcec1e26d2e4e635dc9545a534c922cc
RichGlitch <rich@glitch.com>
2016-02-23 05:51:42 +0000

    add gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,29 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Welcome to the blog #{user.username}'
+      flash[:success] = 'Welcome to the blog #{@user.username}'
       redirect_to articles_path
     else
       render 'new'
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = ' #{@user.username} updated'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   private
