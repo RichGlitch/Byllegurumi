5f40a6e5dfafd9e56c87455e39cc5dd8b132874c
RichGlitch <rich@glitch.com>
2016-02-24 00:15:51 +0000

    creador de articulo

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user =current_user
     if @article.save
       flash[:success] = "Articulo ha sido creado exitosamente."
       redirect_to article_path(@article)
