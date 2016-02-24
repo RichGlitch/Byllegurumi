146565a35bcba2f2037f57fadb00b4ee3c83e15a
RichGlitch <rich@glitch.com>
2016-02-24 17:51:03 +0000

    Asociacion articulo y categoria

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :titulo, presence: true, length: {minimum: 3, maximum: 50}
   validates :patron, presence: true, length: {minimum: 10, maximum: 5000}
   validates :subtitulo, presence: true, length: {minimum: 3, maximum: 50}
